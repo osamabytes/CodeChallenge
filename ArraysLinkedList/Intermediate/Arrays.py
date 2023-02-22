34b2dbfcbdd98deaefa8862cb43d24634f4254aa
OsamaBytes <osama.ahmed@46bytes.com>
2023-02-22 18:16:44 +0500

    2. Find the missing number in an array of consecutive integers.     3. Merge two sorted arrays into a third sorted array.

--- ArraysLinkedList/Intermediate/Arrays.py
+++ ArraysLinkedList/Intermediate/Arrays.py
@@ -1,6 +1,6 @@
 import sys
 
-main_list = [2, 1, 2, 2, 1, 4, 3, 3, 6, 8, 10, 8]
+main_list = [2, 1, 2, 2, 1, 4, 3, 3, 6, 8, 12, 8]
 
 #functions
 def RemoveRedundancy(list = []):
@@ -13,10 +13,45 @@ def RemoveRedundancy(list = []):
         return new_list
     else:
         return list
+def GetConsecutives(para_list = []):
+    if(len(para_list) > 0):
+        new_list = []
+        for i in range(len(para_list)):
+            if(i < len(para_list) - 1):
+                # find the difference and then add the consecutive nums
+                curr_num = para_list[i]
+                next_num = para_list[i + 1]
+                difference = next_num - curr_num
+                if(difference > 1):
+                    new_list += list(range(curr_num + 1, next_num))
+        return new_list
+    else:
+        return para_list
+def GetMergedListofTwoLists(list1 = [], list2 = []):
+    if len(list1) == 0 or len(list2) == 0:
+        return []
+    new_list = list1 + list2
+    new_list.sort()
+    return new_list
 #end functions
 
 #Remove duplicates from an unsorted array.
-main_list = RemoveRedundancy(main_list)
-if len(main_list) == 0:
+redundant_list = RemoveRedundancy(main_list)
+if len(redundant_list) == 0:
     print("Add data to List. Empty list is not allowed for further processing")
     sys.exit()    
+print(redundant_list)
+
+#Consecutive Arrays
+consecutive_arrays = GetConsecutives(redundant_list)
+if len(consecutive_arrays) == 0:
+    print("Add data to List. Empty list is not allowed for further processing")
+    sys.exit()    
+print(consecutive_arrays)
+
+#MergeList
+merge_list = GetMergedListofTwoLists([2,4,1], [7,3,8,9])
+if len(merge_list) == 0:
+    print("Add data to List. Empty list is not allowed for further processing")
+    sys.exit()    
+print(merge_list)
